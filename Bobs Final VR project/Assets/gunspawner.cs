f8fe151a909f9497a3ffd87b6a926d67c934b012
Bobby <rschloth@scu.edu>
2018-10-30 20:06:23 -0700

    Initial Push

---
+++ Bobs Final VR project/Assets/gunspawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snowballspawner : MonoBehaviour {
+    public Transform _Gun;
+    public GameObject _Snowball;
+    public Rigidbody _Gunrb;
+    public float thrust = 10;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Spawnsnowball();
+        }
+        
+
+        
+
+        
+    }
+
+
+
+    public void Spawnsnowball()
+    {
+        Instantiate(_Snowball,_Gun);
+       _Gunrb = _Snowball.GetComponent<Rigidbody>();
+        _Gunrb.AddForce(transform.forward * thrust);
+    }
+
+}
